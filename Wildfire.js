e12ce8c9e4a83338bbee67d89dc36742b563cb47
Scott Belcher <scottbelcher@live.com.au>
2017-08-24 20:11:49 +0930

    added 6 new pages and started js

---
+++ Wildfire.js
@@ -0,0 +1,19 @@
+
+
+/**Mobile Menu**/
+  /*up*/
+    $(document).ready(function(){
+      $("#mobile-menu-trigger").click(function(){
+        $("#mobile-menu").animate({top:'10vh'})
+        $('#mobile-menu').css({display:'block'});
+      });
+    });
+  /*down*/
+    $(document).ready(function(){
+      $("#mobile-menu-close").click(function(){
+          $("#mobile-menu").animate({top:'-110vh'},function(){
+            $("#mobile-menu").css({display:'none'});
+          });
+        });
+      });
+/**NEXT**/
